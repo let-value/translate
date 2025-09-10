fd8362941322327da1798881be7c81b0874fcf4c
Alexander Chehovskii <alexander.chehoff@gmail.com>
2025-09-10 21:47:04 +0500

    trim react message whitespace

--- extract/src/plugins/react/tests/fixtures/valid.tsx
+++ extract/src/plugins/react/tests/fixtures/valid.tsx
@@ -4,6 +4,10 @@ import { Fragment } from "react";
 const name = "World";
 const n = 2;
 
+const cookiePolicy = <span>{message`Cookie Policy`}</span>;
+const termsOfService = <span>{message`Terms of Service`}</span>;
+const privacyPolicy = <span>{message`Privacy Policy`}</span>;
+
 <Message>hello</Message>;
 <Message>hello {name}</Message>;
 <Message context="verb">run</Message>;
@@ -11,3 +15,6 @@ const n = 2;
 <Plural number={1} forms={[<Fragment>one</Fragment>, <Fragment>many</Fragment>]} />;
 <Plural number={n} forms={[<Fragment>One {name}</Fragment>, <Fragment>Many {name}s</Fragment>]} context="count" />;
 <Plural number={n} forms={["simple one", "simple many"]} />;
+<Message>
+    {termsOfService} • {privacyPolicy} • {cookiePolicy}
+</Message>;
